d57d8f45122d13895259f0b9fc559601c12f56aa
Afridi Majeed <afridi.majeed97@gmail.com>
2020-10-05 09:23:31 +0530

    assignments

---
+++ programming_assignment2/5_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+// structure for message queue
+struct msg_buffer {
+   long msg_type;
+   char msg[100];
+} message;
+int main() {
+   key_t my_key;
+   int msg_id;
+   my_key = ftok("progfile", 65); //create unique key
+   msg_id = msgget(my_key, 0666 | IPC_CREAT); //create message queue and return id
+   message.msg_type = 1;
+   printf("Enter Command: ");
+   fgets(message.msg, 100, stdin);
+   msgsnd(msg_id, &message, sizeof(message), 0); //send message
+   msgrcv(msg_id, &message, sizeof(message), 1, 0); 
+   return 0;
+}
+
